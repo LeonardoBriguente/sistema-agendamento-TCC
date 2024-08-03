fd74e19eb4aec754608820b5624d19a8b1eb9afa
Leonardo Briguente <147267206+LeonardoBriguente@users.noreply.github.com>
2024-08-02 21:19:05 -0300

    Add files via upload

--- javascript/NewCadastro.js
+++ javascript/NewCadastro.js
@@ -1,11 +1,92 @@
-class Formulario{
+class FormActions{
     constructor(){
-        this.allInputs = document.querySelectorAll('input');
+        this.inputs = document.querySelectorAll('input');
         this.eye = document.getElementById('icone');
-        this.password = document.getElementById('inputPassword');
-        this.reviewPass = document.getElementById('review-password');
-        this.alert = document.getElementById('aviso');
+        this.passwordInput = document.getElementById('inputPassword');
+        this.reviewPassInput = document.getElementById('review-password');
+        this.alertMessege = document.getElementById('aviso');
+
+        this.addFocusListeners();
+        this.addEyeListener();
     }
 
+    addFocusListeners(){
+        this.inputs.forEach(input => {
+            input.addEventListener('focus', function(){
+                this.style.backgroundColor = "";
+            });
+        });
+    }
+
+    addEyeListener(){
+        this.eye.addEventListener('click', () => this.ShowPassword());
+    }
     
+    ShowPassword(){
+        if(this.passwordInput.type == "password"){
+            this.passwordInput.type = "text";
+            this.reviewPassInput.type = "text";
+            this.eye.src = "../imagens/olho-aberto.png";
+        }
+        else{
+            this.passwordInput.type = "password";
+            this.reviewPassInput.type = "password";
+            this.eye.src = "../imagens/olho-fechado.png";
+        }
+    }
+
+    VerifyAllInputs() {
+        let verifyComplete = true;
+
+        this.inputs.forEach(input => {
+            if (input.value.trim() === '') {
+                input.style.backgroundColor = "rgba(33, 106, 86, 0.2)";
+                input.style.borderBottomColor = "#216A56";
+                verifyComplete = false;
+            }
+        });
+
+        console.log(verifyComplete);
+        return verifyComplete;
+    }
+
+    VerifyPasswords() {
+
+        //console de teste ATENÇÃO
+        console.log("entrou");
+        let passwordValue = this.passwordInput.value;
+        let reviewValue = this.reviewPassInput.value;
+
+        console.log(passwordValue);
+        console.log(reviewValue);
+
+        if (passwordValue != reviewValue) {
+        console.log("entrou if");
+
+            this.alertMessage.style.display = "block";
+            this.reviewPassInput.style.backgroundColor = "rgba(33, 106, 86, 0.2)";
+            this.reviewPassInput.style.borderBottomColor = "#216A56";
+
+            return false;
+        }
+        console.log("passou");
+
+        return true;
+    }
+
+    VerifyForm() {
+        if(this.VerifyPasswords() == true && this.VerifyAllInputs() == true){
+            return true;
+        }
+
+        else{
+            return false;
+        }
+        // console.log("Verifying form...");
+        // return  this.VerifyPasswords() && this.VerifyAllInputs();
+
+        //PROBELMAS: Se colocar qualquer coisa nas senhas o form vai de arrasta mesmo com campos vazios.
+    }
 }
+
+let formAction = new FormActions();
--- telas/cadastro.php
+++ telas/cadastro.php
@@ -8,16 +8,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/cadastro.css">
-    <script type="text/javascript" src="../javascript/cadastro.js" defer></script>
+    <script type="text/javascript" src="../javascript/NewCadastro.js" defer></script>
+    <script src="https://kit.fontawesome.com/553722128b.js" crossorigin="anonymous"></script>
     <title>Cadastro</title>
 </head>
 
 <body>
     <section class="container">
         <div class="box">
-            <img class="seta" src="../imagens/seta-esquerda.png">
+            <svg class="seta" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.5.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg>
             <h3 class="messege">Preencha as informações para efetuar seu cadastro</h3>
-            <form method="post" action="../processamento/processamento.php" id="formulario" onsubmit="return VerifyForm()">
+            <form method="post" action="../processamento/processamento.php" id="formulario" onsubmit="return formAction.VerifyForm()">
                 <label class="informacoes" for="inputName">Nome:</label>
                 <input class="entrada-dados" type="text" id="inputName" name="inputName" placeholder="Digite seu nome" >
 
@@ -37,7 +38,7 @@
                 <input class="entrada-dados" type="password" id="review-password" name="review-password" placeholder="Digite sua senha">
 
                 <!--AJUSTAR RESPONSIVIDADE (location) OLHO, principalmente nas mensagem de erro-->
-                <img src="../imagens/olho-fechado.png" id="icone" class="olho" onclick="ShowPassword()">
+                <img src="../imagens/olho-fechado.png" id="icone" class="olho" >
 
                 <span id="aviso">*As senhas não correspondem</span>
 
