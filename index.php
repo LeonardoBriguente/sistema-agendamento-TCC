c3647eee9965e49bb832c81f7e619ce8b601920b
Leonardo Briguente <leobriguente27@gmail.com>
2024-08-08 11:04:15 -0300

    Customização do botão e adição de olho na senha

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="./view/assets/css/index.css">
     <script src="https://kit.fontawesome.com/553722128b.js" crossorigin="anonymous"></script>
     <title>Bem vindo ao Stima Studio</title>
@@ -15,23 +16,24 @@
             <section class="welcome">
                 <img src="./view/assets/images/logo-studio.png">
                 <h2 class="text-welcome">Bem vindo ao Stima Studio</h2>
-                
+
             </section>
 
             <section class="form">
                 <form>
                     <div class="input-box">
                         <i class="fa-regular fa-envelope espaco"></i>
-                        <input type="text" id="inputEmail" name="inputEmail" placeholder="E-mail" required>
+                        <input type="email" id="inputEmail" name="inputEmail" placeholder="E-mail" required>
                     </div>
 
                     <!-- Verificar tamanho das Divs -->
                     <div class="input-box">
                         <i class="fa-solid fa-lock espaco"></i>
                         <input type="password" id="inputPassword" name="inputPassword" placeholder="Senha">
+                        <i class="fa-solid fa-eye-slash"></i>
                     </div>
 
-                    <button type="submit">Entrar</button>
+                    <button type="submit">ENTRAR</button>
                 </form>
             </section>
         </main>
