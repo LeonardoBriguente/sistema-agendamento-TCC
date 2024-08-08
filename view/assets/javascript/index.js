f21096d20d123171019d4fef54af9250cf08c91e
Leonardo Briguente <leobriguente27@gmail.com>
2024-08-08 20:00:28 -0300

    Finalização do JS com funções de ver senha e troca de elementos visuais

--- view/assets/javascript/index.js
+++ view/assets/javascript/index.js
@@ -4,14 +4,22 @@ class Usuario{
         this.password = document.getElementById("inputPassword");
 
         this.ModifyEye();
-        this.ShowPassword();
     };
 
     ModifyEye(){
-        
+        this.eye.addEventListener("click", () => this.ShowPassword());
     }
 
     ShowPassword(){
-
+        if(this.password.type == "password"){
+            this.password.type = "text";
+            this.eye.className = "fa-solid fa-eye eye";
+        }
+        else{
+            this.password.type = "password";
+            this.eye.className = "fa-solid fa-eye-slash eye";
+        }
     }
 }
+
+let user = new Usuario();
