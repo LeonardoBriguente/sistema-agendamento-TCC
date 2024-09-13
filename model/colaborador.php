84f36753d1ec4237097531d9772576d7cf27f7b8
Leonardo Briguente <leobriguente27@gmail.com>
2024-09-13 20:56:32 -0300

    elaboração da class Colaborador

--- model/colaborador.php
+++ model/colaborador.php
@@ -1,5 +1,33 @@
 <?php
-class Colaborador{
-    private $email;
-    
+require_once "./database.php";
+
+class Colaborador
+{
+    private $pdo;
+
+    public function __construct()
+    {
+        $database = new Database("localhost", "stimastudio", "root", "");
+        $this->pdo = $database->Connect();
+    }
+
+    public function AddColaborador($nome, $logradouro, $data_nascimento, $bairro, $cidade, $cpf, $telefone, $email, $complemento)
+    {
+        $sql = "INSERT INTO colaborador (nome, logradouro, data_nascimento, bairro, cidade, cpf, telefone, email, complemento)
+                VALUES (:nome, :logradouro, :data_nascimento, :bairro, :cidade, :cpf, :telefone, :email, :complemento)";
+
+        $stmt = $this->pdo->prepare($sql);
+
+        $stmt->bindParam(':nome', $nome);
+        $stmt->bindParam(':logradouro', $logradouro);
+        $stmt->bindParam(':data_nascimento', $data_nascimento);
+        $stmt->bindParam(':bairro', $bairro);
+        $stmt->bindParam(':cidade', $cidade);
+        $stmt->bindParam(':cpf', $cpf);
+        $stmt->bindParam(':telefone', $telefone);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':complemento', $complemento);
+
+        $stmt->execute();
+    }
 }
