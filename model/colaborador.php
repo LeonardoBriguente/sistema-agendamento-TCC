83e30b45e8784214b2526f57d8a7c93994b8e2bd
Leonardo Briguente <leobriguente27@gmail.com>
2024-10-06 19:21:19 -0300

    Auto organização dos códigos

--- model/colaborador.php
+++ model/colaborador.php
@@ -13,31 +13,30 @@ class Colaborador
     }
 
     public function AddColaborador($nome, $logradouro, $numero_residencia, $data_nascimento, $bairro, $cidade, $cpf, $telefone, $email, $complemento)
-{
-    try {
-        $sql = "INSERT INTO colaborador (nome, logradouro, numero_residencia, data_nascimento, bairro, cidade, cpf, telefone, email, complemento)
+    {
+        try {
+            $sql = "INSERT INTO colaborador (nome, logradouro, numero_residencia, data_nascimento, bairro, cidade, cpf, telefone, email, complemento)
                 VALUES (:nome, :logradouro, :numero_residencia, :data_nascimento, :bairro, :cidade, :cpf, :telefone, :email, :complemento)";
 
-        $stmt = $this->pdo->prepare($sql);
+            $stmt = $this->pdo->prepare($sql);
 
-        $stmt->bindParam(':nome', $nome);
-        $stmt->bindParam(':logradouro', $logradouro);
-        $stmt->bindParam(':numero_residencia', $numero_residencia);
-        $stmt->bindParam(':data_nascimento', $data_nascimento);
-        $stmt->bindParam(':bairro', $bairro);
-        $stmt->bindParam(':cidade', $cidade);
-        $stmt->bindParam(':cpf', $cpf);
-        $stmt->bindParam(':telefone', $telefone);
-        $stmt->bindParam(':email', $email);
-        $stmt->bindParam(':complemento', $complemento);
+            $stmt->bindParam(':nome', $nome);
+            $stmt->bindParam(':logradouro', $logradouro);
+            $stmt->bindParam(':numero_residencia', $numero_residencia);
+            $stmt->bindParam(':data_nascimento', $data_nascimento);
+            $stmt->bindParam(':bairro', $bairro);
+            $stmt->bindParam(':cidade', $cidade);
+            $stmt->bindParam(':cpf', $cpf);
+            $stmt->bindParam(':telefone', $telefone);
+            $stmt->bindParam(':email', $email);
+            $stmt->bindParam(':complemento', $complemento);
 
-        $stmt->execute();
-        
-        return "Colaborador adicionado com sucesso!";
-    } catch (PDOException $e) {
+            $stmt->execute();
 
-        return "Erro ao adicionar colaborador: " . $e->getMessage();
+            return "Colaborador adicionado com sucesso!";
+        } catch (PDOException $e) {
+
+            return "Erro ao adicionar colaborador: " . $e->getMessage();
+        }
     }
-}
-
 }
